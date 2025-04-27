bc3a59b1808f6de0bf9556a4bfe9b3ba372533da
macalistervadim <macalistervadim@yandex.ru>
2025-04-27 14:04:06 +0600

    feat(docker): add cerbot, add nginx / feat(main): update HTTP to HTTPS

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ BACKEND_CORS_ORIGINS = os.getenv("BACKEND_CORS_ORIGINS", "").split(",")
 
 s3 = boto3.client(
     "s3",
-    endpoint_url=f"http://{MINIO_ENDPOINT}",
+    endpoint_url=f"https://{MINIO_ENDPOINT}",
     aws_access_key_id=MINIO_ACCESS_KEY,
     aws_secret_access_key=MINIO_SECRET_KEY,
     config=Config(signature_version="s3v4"),
@@ -71,7 +71,7 @@ async def upload_file(file: UploadFile = File(...)):
             Body=content,
             ContentType=file.content_type,
         )
-        file_url = f"http://{os.getenv('PUBLIC_MINIO_URL')}/{MINIO_BUCKET_NAME}/{file.filename}"
+        file_url = f"https://{os.getenv('PUBLIC_MINIO_URL')}/{MINIO_BUCKET_NAME}/{file.filename}"
         return JSONResponse(content={"url": file_url})
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
